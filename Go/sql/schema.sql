bab8d56d9161a1f0ef633d0530043e44fb807295
DiegoN96 <diegonavarroarizpe@hotmail.com>
2025-06-05 14:59:55 -0500

    Changed Shema

--- Go/sql/schema.sql
+++ Go/sql/schema.sql
@@ -1,5 +1,23 @@
+-- Create sequence for organizations table
+CREATE SEQUENCE IF NOT EXISTS public.organizations_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+-- Tabla Organizations
+CREATE TABLE IF NOT EXISTS public.organizations
+(
+    id integer NOT NULL DEFAULT nextval('organizations_id_seq'::regclass),
+    name text COLLATE pg_catalog."default" NOT NULL,
+    created_at timestamp without time zone NOT NULL DEFAULT now(),
+    updated_at timestamp without time zone NOT NULL DEFAULT now(),
+    CONSTRAINT organizations_pkey PRIMARY KEY (id)
+);
+
 -- Tabla Usuarios con organización y rol
-CREATE TABLE public."Users"
+CREATE TABLE IF NOT EXISTS public."Users"
 (
     id SERIAL PRIMARY KEY,
     username TEXT NOT NULL,
@@ -10,14 +28,4 @@ CREATE TABLE public."Users"
     organization_id INT NOT NULL REFERENCES public.organizations(id),
     created_at TIMESTAMP NOT NULL DEFAULT NOW(),
     updated_at TIMESTAMP NOT NULL DEFAULT NOW()
-);
-
-
-CREATE TABLE public.organizations
-(
-    id integer NOT NULL DEFAULT nextval('organizations_id_seq'::regclass),
-    name text COLLATE pg_catalog."default" NOT NULL,
-    created_at timestamp without time zone NOT NULL DEFAULT now(),
-    updated_at timestamp without time zone NOT NULL DEFAULT now(),
-    CONSTRAINT organizations_pkey PRIMARY KEY (id)
 );
